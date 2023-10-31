85d27e6b3e0a644b77d0684548bbca4c7cef8017
FREEDOM <freewhynane62@gmail.com>
2023-10-31 03:24:54 +0000

    add dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["react-app", "react-app/jest"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  globals: {
+    chrome: "writable",
+  },
+
+  settings: {
+    "eslint.validate": ["javascript", "javascriptreact", "chrome"],
+  },
+
+  // Add other custom ESLint rules or configuration as needed
+};

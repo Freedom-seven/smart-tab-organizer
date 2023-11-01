6dffd4a8d547cf0677f6929b94f7241029a6e34e
FREEDOM <freewhynane62@gmail.com>
2023-11-01 03:21:19 +0000

    remove

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    }),
-  ],
-};
